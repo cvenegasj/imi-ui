54f413534b10c0be8e79ca18524607b4630356a0
Carlos Venegas <cvj_2311@hotmail.com>
2021-07-15 10:03:09 -0500

    changed resApiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  resApiUrl: 'https://res.symbiocreation.net', // has to be https (port 443 by default)
+  resApiUrl: 'http://imi-res-env.eba-pmhe8ihf.us-east-1.elasticbeanstalk.com', // has to be https (port 443 by default)
   localhost: 'https://app.simbiocreacion.com',
 };
